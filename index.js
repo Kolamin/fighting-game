564915eda20d90ad8d4ae3cb46b77cb005ac640f
Kolamin <gofranich@gmail.com>
2023-07-20 09:24:30 +0600

    finish run

--- index.js
+++ index.js
@@ -46,6 +46,17 @@ const player = new Fighter({
     x: 215,
     y: 157,
   },
+  sprites: {
+    idle: {
+      imageSrc: "./img/samuraiMack/Idle.png",
+      frameMax: 8,
+    },
+    run: {
+      imageSrc: "./img/samuraiMack/Run.png",
+      frameMax: 8,
+      image: new Image(),
+    },
+  },
 });
 
 const enemy = new Fighter({
@@ -64,8 +75,6 @@ const enemy = new Fighter({
     y: 0,
   },
 });
-
-console.log(player);
 
 const keys = {
   a: {
@@ -103,10 +112,13 @@ function animate() {
   enemy.velocity.x = 0;
 
   //player movement
+  player.image = player.sprites.idle.image;
   if (keys.a.pressed && player.lastKey === "a") {
     player.velocity.x = -5;
+    player.image = player.sprites.run.image;
   } else if (keys.d.pressed && player.lastKey === "d") {
     player.velocity.x = 5;
+    player.image = player.sprites.run.image;
   }
 
   //enmy movement
